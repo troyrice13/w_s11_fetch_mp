f270bc557acc159fcf2537eb27f9cefcd7af3d73
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-04-19 16:11:48 +0200

    change breeds

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ server.use(cors())
 
 let id = 1
 const getNextId = () => id++
-const dogBreeds = ['Labrador Retriever', 'German Shepherd', 'Golden Retriever', 'French Bulldog', 'Bulldog']
+const dogBreeds = ['Dalmatian', 'German Shepherd', 'Golden Retriever', 'Boxer', 'Bulldog']
 let dogs = [
   {
     id: getNextId(),
