43bf6812f62683592e92f6ce223b13f153e8e479
Troy Rice <troyanthonyrice@gmail.com>
2024-05-02 13:17:08 +0200

    save

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Routes, Route, NavLink } from 'react-router-dom'
 import DogForm from './DogForm'
 import DogsList from './DogsList'
 
+
+
+
 export default function App() {
+  const [dogs, setDogs] = useState([])
+  const [currentDogId, setCurrentDog] = useState(null)
+
+  useEffect(() => {
+    getDogs()
+  }, [])
+
+
+  const getDogs = () => {
+    fetch('api/dogs')
+    .then(res => {
+      if (!res.ok) throw new Error('Network response not OK')
+      return res.json()
+    })
+    .then(setDogs)
+    .catch(err => console.error(err))
+  }
+
   return (
     <div>
       <nav>
@@ -11,8 +32,14 @@ export default function App() {
         <NavLink to="/form">Form</NavLink>
       </nav>
       <Routes>
-        <Route path="/" element={<DogsList />} />
-        <Route path="/form" element={<DogForm />} />
+        <Route path="/" element={<DogsList
+          dogs={dogs} setCurrentDog={setCurrentDog} getDogs={getDogs}
+        />} />
+        <Route path="/form" element={<DogForm 
+          dog={currentDogId && dogs.find(d => d.id == currentDogId)}
+          getDogs={getDogs}
+          reset={() => setCurrentDog(null)}
+        />} />
       </Routes>
     </div>
   )
--- frontend/components/DogsList.js
+++ frontend/components/DogsList.js
@@ -1,17 +1,36 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export default function DogsList() {
+export default function DogsList({ dogs, setCurrentDog, getDogs }) {
+  const navigate = useNavigate()
+  const editDog = id => {
+    setCurrentDog(id)
+    navigate('form')
+  }
+
+  const deleteDog = id => {
+    fetch(`api/dogs/${id}`, { method: 'DELETE' })
+    .then(() => getDogs())
+    .then(() => setCurrentDog(null))
+    .catch(err => console.error('Failed to delete dog', err))
+  }
+
   return (
     <div>
       <h2>Dogs Shelter</h2>
       <ul>
-        <li>
-          Fido, Bulldog, NOT adopted
+        {
+          dogs.map(dog => (
+        
+        <li key={dog.id}>
+          {dog.name}, {dog.breed}, {dog.adopted ? '' : 'NOT '}adopted
           <div>
-            <button>Edit</button>
-            <button>Delete</button>
+            <button onClick={() => editDog(dog.id)}>Edit</button>
+            <button onClick={() => deleteDog(dog.id)}>Delete</button>
           </div>
         </li>
+        ))
+      }
       </ul>
     </div>
   )
